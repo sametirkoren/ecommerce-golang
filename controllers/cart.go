1ea789e3bcdcc6a8159ce9e3970c0f6f8606650f
samet irkören <sametirkoren@gmail.com>
2022-09-03 22:26:43 +0300

    controllers added

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,1 +1,17 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AddToCart() gin.HandlerFunc {
+
+}
+
+func RemoteItem() gin.HandlerFunc {
+
+}
+
+func GetItemFromCart() gin.HandlerFunc {
+
+}
